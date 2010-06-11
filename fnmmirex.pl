d6ff6e3bd88f57b377fb96f343e26addc94b2d2c
ade ishs <ade@adeishs.com>
2010-06-11 13:41:00 +0000

    Index construction and caching works.

--- fnmmirex.pl
+++ fnmmirex.pl
@@ -25,11 +25,38 @@ sub query($$) {
 
 sub create_index($) {
     my $coll_dir = shift;
+    my $index_dir = "$coll_dir/$FNM_DIR";
+    my @cmd;
+    my $result;
+
+    # create index dir if not existing
+    unless (-d $index_dir) {
+        mkdir $index_dir or die "Can't create $index_dir\n";
+    }
 
     # parse collection files and generate sequence file
+    my $temp_seq_fn = "$index_dir/.fnmmp.seq.$$";
+    @cmd = ('./fnmmp.pl', $coll_dir, $temp_seq_fn);
+    print "@cmd\n";
+    $result = system @cmd;
 
+    if ($result != 0 || !(-f $temp_seq_fn)) {
+        return 0;
+    }
     # index the sequences in the sequence file
+    my $index_name = "$index_dir/index";
+    @cmd = ('./fnmib', $index_name, $temp_seq_fn);
+    $result = system @cmd;
 
+    if ($result != 0 ||
+        !(-f "$index_name.fdl") ||
+        !(-f "$index_name.filp") ||
+        !(-f "$index_name.fipp")) {
+        return 0;
+    }
+    unlink $temp_seq_fn;
+
+    return 1;
 }
 
 sub index_found($) {
@@ -51,11 +78,13 @@ fnmmirex.pl /path/to/coll/files/dir/ /path/to/query.mid
 
 The collection files must be in MIDI format.
 EOT
+
+    exit 1;
 }
 
 sub main {
     if (@ARGV != $EXPECTED_ARGC) {
-        usage();
+        print_usage();
         exit 1;
     }
 
